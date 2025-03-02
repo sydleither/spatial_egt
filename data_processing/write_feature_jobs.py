1c5e27b25d98b9282fa6898ada7107c5db52c601
sydleither <sydleither@protonmail.com>
2025-03-02 09:44:14 -0500

    fix spaces in feature names (breaking sbatch)

--- data_processing/write_feature_jobs.py
+++ data_processing/write_feature_jobs.py
@@ -12,59 +12,59 @@ from data_processing.spatial_statistics.muspan import (anni, entropy, cpcf, cros
 
 FEATURE_REGISTRY = {
     # Custom
-    "NC (Resistant)": nc_dist,
-    "NC (Sensitive)": nc_dist,
-    "Proportion Sensitive": proportion_s,
+    "NC_(Resistant)": nc_dist,
+    "NC_(Sensitive)": nc_dist,
+    "Proportion_Sensitive": proportion_s,
     "SFP": sfp_dist,
     # MuSpAn
     "ANNI": anni,
     "Entropy": entropy,
     "CPCF": cpcf,
-    "Cross Ripleys k": cross_k,
-    "J Function": j_function,
-    "KL Divergence": kl_divergence,
-    "Global Morans i": global_moransi,
-    "Local Morans i": local_moransi_dist,
+    "Cross_Ripleys_k": cross_k,
+    "J_Function": j_function,
+    "KL_Divergence": kl_divergence,
+    "Global_Morans_i": global_moransi,
+    "Local_Morans_i": local_moransi_dist,
     "NN": nn_dist,
     "SES": qcm,
     "Wasserstein": wasserstein,
     # Temp
-    "Patch Circularity": circularity,
-    "Patch Fractal Dim": fractal_dimension
+    "Patch_Circularity": circularity,
+    "Patch_Fractal_Dim": fractal_dimension
 }
 
 FEATURE_PARAMS = {
     "in_silico": {
-        "NC (Resistant)": {"radius": 3, "return_fs": True},
-        "NC (Sensitive)": {"radius": 3, "return_fs": False},
+        "NC_(Resistant)": {"radius": 3, "return_fs": True},
+        "NC_(Sensitive)": {"radius": 3, "return_fs": False},
         "SFP": {"sample_length": 5},
-        "Cross Ripleys k": {"max_radius": 5, "step": 1},
+        "Cross_Ripleys_k": {"max_radius": 5, "step": 1},
         "CPCF": {"max_radius": 5, "annulus_step": 1, "annulus_width": 3},
-        "J Function": {"cell_type": "sensitive", "radius_step": 2},
-        "KL Divergence": {"mesh_step": 3},
-        "Global Morans i (Resistant)": {"cell_type": "resistant", "side_length": 5},
-        "Global Morans i (Sensitive)": {"cell_type": "sensitive", "side_length": 5},
-        "Local Morans i (Resistant)": {"cell_type": "resistant", "side_length": 5},
-        "Local Morans i (Sensitive)": {"cell_type": "sensitive", "side_length": 5},
+        "J_Function": {"cell_type": "sensitive", "radius_step": 2},
+        "KL_Divergence": {"mesh_step": 3},
+        "Global_Morans_i (Resistant)": {"cell_type": "resistant", "side_length": 5},
+        "Global_Morans_i (Sensitive)": {"cell_type": "sensitive", "side_length": 5},
+        "Local_Morans_i (Resistant)": {"cell_type": "resistant", "side_length": 5},
+        "Local_Morans_i (Sensitive)": {"cell_type": "sensitive", "side_length": 5},
         "SES": {"side_length": 10},
-        "Patch Circularity": {"cell_type": "sensitive", "alpha": 3},
-        "Patch Fractal Dim": {"cell_type": "sensitive", "alpha": 3}
+        "Patch_Circularity": {"cell_type": "sensitive", "alpha": 3},
+        "Patch_Fractal_Dim": {"cell_type": "sensitive", "alpha": 3}
     },
     "in_vitro": {
-        "NC (Resistant)": {"radius": 30, "return_fs": True},
-        "NC (Sensitive)": {"radius": 30, "return_fs": False},
+        "NC_(Resistant)": {"radius": 30, "return_fs": True},
+        "NC_(Sensitive)": {"radius": 30, "return_fs": False},
         "SFP": {"sample_length": 50},
-        "Cross Ripleys k": {"max_radius": 50, "step": 10},
+        "Cross_Ripleys_k": {"max_radius": 50, "step": 10},
         "CPCF": {"max_radius": 50, "annulus_step": 10, "annulus_width": 30},
-        "J Function": {"cell_type": "sensitive", "radius_step": 20},
-        "KL Divergence": {"mesh_step": 30},
-        "Global Morans i (Resistant)": {"cell_type": "resistant", "side_length": 50},
-        "Global Morans i (Sensitive)": {"cell_type": "sensitive", "side_length": 50},
-        "Local Morans i (Resistant)": {"cell_type": "resistant", "side_length": 50},
-        "Local Morans i (Sensitive)": {"cell_type": "sensitive", "side_length": 50},
+        "J_Function": {"cell_type": "sensitive", "radius_step": 20},
+        "KL_Divergence": {"mesh_step": 30},
+        "Global_Morans_i (Resistant)": {"cell_type": "resistant", "side_length": 50},
+        "Global_Morans_i (Sensitive)": {"cell_type": "sensitive", "side_length": 50},
+        "Local_Morans_i (Resistant)": {"cell_type": "resistant", "side_length": 50},
+        "Local_Morans_i (Sensitive)": {"cell_type": "sensitive", "side_length": 50},
         "SES": {"side_length": 100},
-        "Patch Circularity": {"cell_type": "sensitive", "alpha": 30},
-        "Patch Fractal Dim": {"cell_type": "sensitive", "alpha": 30}
+        "Patch_Circularity": {"cell_type": "sensitive", "alpha": 30},
+        "Patch_Fractal_Dim": {"cell_type": "sensitive", "alpha": 30}
     }
 }
 
@@ -73,7 +73,7 @@ def main(data_type, run_local):
     run_cmd = "python3 -m" if run_local else "sbatch job.sb"
     output = []
     for feature_name in FEATURE_REGISTRY.keys():
-        output.append(f"{run_cmd} data_processing.processed_to_feature \"{feature_name}\" {data_type}\n")
+        output.append(f"{run_cmd} data_processing.processed_to_feature {feature_name} {data_type}\n")
     with open("process_features.sh", "w") as f:
         for output_line in output:
             f.write(output_line)
